76f8f09fce6012caed92ffe41d3373b196297aff
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-21 17:48:33 +0200

    Solve tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const CONVERSION_FACTOR = 5/9;
+const C_ZERO_IN_F = 32;
+
+function roundTo1stPlace(num){
+  return Math.round(num*10)/10;
+}
+
+const convertToCelsius = function(temp) {
+  return roundTo1stPlace((temp-C_ZERO_IN_F)*CONVERSION_FACTOR);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return roundTo1stPlace(temp/CONVERSION_FACTOR+C_ZERO_IN_F);
 };
 
 // Do not edit below this line
