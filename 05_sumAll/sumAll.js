29c646cb172550b42fa66ede2ae4164ea350de01
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-21 17:18:41 +0200

    Solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+function calcGauss(num){
+    return (num * ( num + 1))/2;
+}
 
+function checkPositiveInteger(num){
+    if(typeof(num) === 'number' && Math.floor(num) === num && num > 0)
+        return true;
+    return false;
+}
+
+const sumAll = function(num1, num2) {
+    if((!checkPositiveInteger(num1)) || (!checkPositiveInteger(num2)))
+        return "ERROR";
+    return calcGauss(Math.max(num1, num2)) - calcGauss(Math.min(num1, num2)-1);
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
