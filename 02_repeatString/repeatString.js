ea577a881b0b67f3d9c60b7de204434d519d57df
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-21 14:40:31 +0200

    Solve RepeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) return "ERROR";
+    let resultStr = "";
+    for(let i = 0; i < num; i++){
+        resultStr += str;
+    }
+    return resultStr;
 };
 
 // Do not edit below this line
