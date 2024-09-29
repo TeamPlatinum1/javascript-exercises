5b69b49295a5727422fecf5c049f2dce66871040
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-29 11:15:29 +0200

    Solve exercise 10-fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
+const goldenRatio = (1+Math.sqrt(5))/2;
 
+const fibonacci = function(num) {
+    if(num < 0)
+        return "OOPS";
+    return Math.round(Math.pow(goldenRatio, +num)/Math.sqrt(5));
 };
 
 // Do not edit below this line
