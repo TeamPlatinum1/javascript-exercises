6e6997de00893b3279d18cf439b5d491712675b9
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-29 11:29:22 +0200

    Solve exercise 12-findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+function getAge(person){
+    if("yearOfDeath" in person){
+        return person.yearOfDeath - person.yearOfBirth;
+    } else {
+        return (new Date).getFullYear() - person.yearOfBirth;
+    }
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, person, index) => {
+        return index === 0 ? person :
+        getAge(oldestPerson) > getAge(person) ? oldestPerson : person;
+    }, {});
 };
 
 // Do not edit below this line
