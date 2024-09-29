8a77fe2aabb1635e3ca6ad16d8464596a63aedde
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-29 10:57:29 +0200

    Solve exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    array = str.toLowerCase().split("");
+    array = array.filter((value) => (value.charCodeAt(0) >= 0x61 && value.charCodeAt(0) <= 0x7a) ||
+    (value.charCodeAt(0) >= 0x30 && value.charCodeAt(0) <= 0x39));
+    reverseArray = array.slice().reverse();
+    return array.every((value, index) => value === reverseArray[index]);
 };
 
 // Do not edit below this line
