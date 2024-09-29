31f91041d017ba22bfa03e6434c9fcd27791fc72
Thilo Hirscher <176391490+TeamPlatinum1@users.noreply.github.com>
2024-09-29 10:41:00 +0200

    Solve exercise 08-calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, value) => sum + value, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if(arr.length === 0)
+    return 0;
+  return arr.reduce((total, value) => total*value);
 };
 
-const power = function() {
-	
+const power = function(basis, exponent) {
+	return Math.pow(basis, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let total = 1;
+  for(let i = 1; i <= num; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
